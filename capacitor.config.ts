9438b89cde7dffb8230278660b0cb575ec4b3982
Dmytro <dmytro.bor@code-care.pro>
2025-01-06 12:01:48 +0200

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.testb1g1.app",
+  appName: "testb1g1",
+  webDir: "out",
+  server: {
+    androidScheme: "https",
+  },
+};
+
+export default config;
